75270fa60d08acf8b088b9e4b629a31f445edcd2
00134920 <62779299+00134920@users.noreply.github.com>
2020-05-12 17:42:17 -0600

    Add files via upload

--- Promedio.cpp
+++ Promedio.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+int main(){
+    float a,b,c,p;
+
+    cout << "Ingrese la primera nota" << endl;
+    cin >> a;
+    cout << "Ingrese la segunda nota" << endl;
+    cin >> b;
+    cout << "Ingrese la tercera nota" << endl;
+    cin >> c;
+    p = (a + b + c) / 3;
+    cout << "Promedio: " << p << endl;
+    return 0;
+}
